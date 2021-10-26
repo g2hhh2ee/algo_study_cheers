fa790bf2e8b8d98e52606406282672528d380b5e
WzzirrrriYA <58180554+WzzirrrriYA@users.noreply.github.com>
2021-10-27 00:20:39 +0900

    v2

--- day14(21.10.27)/Main_17136_색종이 붙이기_한지희_v2.java
+++ day14(21.10.27)/Main_17136_색종이 붙이기_한지희_v2.java
@@ -1,12 +1,11 @@
-package ext;
-
 import java.util.Scanner;
 
 public class b17136_색종이붙이기_1027 {
 	static int[][] map;
 	static int N = 10;
 	static int min = Integer.MAX_VALUE;
-	static int paper[] = {5, 5, 5, 5, 5};
+	static int paper[] = { 0, 5, 5, 5, 5, 5 };
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		map = new int[N][N];
@@ -15,24 +14,43 @@ public class b17136_색종이붙이기_1027 {
 				map[r][c] = in.nextInt();
 			}
 		}
-		
+
 		dfs(0, 0, 0);
-		if(min == Integer.MAX_VALUE) {
+		if (min == Integer.MAX_VALUE) {
 			System.out.println("-1");
-		}else {
+		} else {
 			System.out.println(min);
 		}
 	}
-	
-	private static void dfs(int i, int j, int k) {
-		
-		
+
+	private static void dfs(int r, int c, int cnt) {
+
+		if (map[r][c] == 1) {// 붙일자리
+			for (int size = 5; size >= 1; size--) {
+				if(paper[size]>0 && check(r,c,size)) {
+					//붙여 (2로 표시)
+					paste(r, c, size);
+					
+					
+				}
+			}
+		}
+	}
+
+	private static void paste(int r, int c, int size) {
+		for (int i = r; i < r + size; i++) {
+			for (int j = c; j < c + size; j++) {
+				map[i][j] = 2;//붙인 거 표시
+			}
+		}
+		//붙였으니까 종이 줄여
+		paper[size]--;
 	}
 
 	public static boolean check(int r, int c, int size) {
 		for (int i = r; i < r + size; i++) {
 			for (int j = c; j < c + size; j++) {
-				if( !inRange(i,j) || map[r][c]==0) {//범위 밖에 있거나 1이 아니면
+				if (!inRange(i, j) || map[r][c] == 0) {// 범위 밖에 있거나 붙일 거 없으면~
 					return false;
 				}
 			}
@@ -41,7 +59,7 @@ public class b17136_색종이붙이기_1027 {
 	}
 
 	private static boolean inRange(int i, int j) {
-		if(i>=0 && j>=0 && i<N && j<N) {
+		if (i >= 0 && j >= 0 && i < N && j < N) {
 			return true;
 		}
 		return false;
